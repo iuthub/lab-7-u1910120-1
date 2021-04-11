bc4b7a64e3ee498923c31b7e4bdf3202fc31c417
u1910120 <s.gulamov@student.inha.uz>
2021-04-11 16:09:00 +0500

    lab 7

---
+++ webpage/login_nav.php
@@ -0,0 +1,36 @@
+<?php
+$isFailedLogin = false;
+if($isPost && $action=='login') {
+    $username = $_REQUEST['username'];
+    $pwd = $_REQUEST['pwd'];
+
+    if ($usersRepo->checkUser($username, $pwd)) {
+        $_SESSION['user'] = $usersRepo->getUser($username);
+        $_SESSION['isAuth'] = TRUE;
+        redirect('index.php');
+    } else {
+        $isFailedLogin = true;
+    }
+}
+?>
+
+<!-- login nav -->
+<div class="nav">
+    <div class="navitem">
+        <?php if ($isFailedLogin): ?>
+            <span class="error">Incorrect login or password.</span>
+        <?php endif ?>
+        <input type="text" name="username" form="loginForm" placeholder="Username" />
+    </div>
+    <div class="navitem">
+        <input type="password" name="pwd" form="loginForm" placeholder="Password" />
+    </div>
+    <div class="navitem">
+        <form id="loginForm" action="index.php?action=login" method="post">
+            <input type="submit" value="Login" />
+        </form>
+    </div>
+    <div class="navitem">
+        <a href="register.php" class="btn">Register</a>
+    </div>
+</div>
---
+++ webpage/logout_nav.php
@@ -0,0 +1,23 @@
+<?php
+$user = $_SESSION['user'];
+if($isPost && $action=='logout') {
+    $_SESSION['isAuth'] = false;
+    redirect('index.php');
+}
+?>
+
+<!-- logout nav -->
+<div class="nav">
+    <div class="navitem navitem-left">
+        Welcome <?= $user['name'] ?>
+    </div>
+    <div class="navitem">
+        <a href="edit.php" class="btn">New Post</a>
+    </div>
+    <div class="navitem">
+        <form action="index.php?action=logout" method="post">
+            <input type="submit" value="Logout" />
+        </form>
+    </div>
+
+</div>
